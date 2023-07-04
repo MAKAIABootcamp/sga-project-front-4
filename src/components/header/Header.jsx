b00aaca111174a011bf14415bac6cec18af99755
luisa urrego <luisafernandaurrego89@gmail.com>
2023-07-04 00:30:07 -0500

    Maquetacion del componente layaout

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import LogoBootcamp from "../../assets/images/LogoBootcamp.png";
+import userProfile from "../../assets/images/profile-img.jpg";
+import { BsList } from "react-icons/bs";
+import "../../styles/header/Header.scss";
+
+const Header = () => {
+  const [hamburguerMenu, setHamburguerMenu] = useState(false);
+  const [listProfileOpen, setListProfileOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setHamburguerMenu(!hamburguerMenu);
+  };
+
+  const handleListProfile = () => {
+    setListProfileOpen(!listProfileOpen);
+  };
+  return (
+    <>
+      <header
+        id="header"
+        className={`header fixed-top d-flex align-items-center ${
+            hamburguerMenu ? "menu-open" : ""
+        }`}
+      >
+        <div className="d-flex align-items-center justify-content-between">
+          <figure className="logo d-flex align-items-center">
+            <img src={LogoBootcamp} alt="Logo" />
+            <span className="d-none d-lg-block">SGA</span>
+            <i
+              className="bi bi-list toggle-sidebar-btn"
+              onClick={handleMenuToggle}
+            >
+              <BsList />
+            </i>
+          </figure>
+        </div>
+        <nav className="header-nav ms-auto">
+          <ul className="d-flex align-items-center">
+            <li className="nav-item dropdown pe-3">
+              <a
+                className="nav-link nav-profile d-flex align-items-center pe-0"
+                data-bs-toggle="dropdown"
+                onClick={handleListProfile}
+              >
+                <img
+                  src={userProfile}
+                  alt="Profile"
+                  className="rounded-circle"
+                />
+                <span className="d-none d-md-block dropdown-toggle ps-2">
+                  K. Anderson
+                </span>
+              </a>
+              {listProfileOpen && (
+                <ul
+                  className={`dropdown-menu dropdown-menu-end dropdown-menu-arrow profile show ${
+                    listProfileOpen ? "menu-open" : ""
+                  }`}
+                  data-popper-placement="bottom-end"
+                  style={{
+                    position: "absolute",
+                    inset: "0px 0px auto auto",
+                    margin: "0px",
+                    transform: "translate3d(-16px, 38px, 0px)",
+                  }}
+                >
+                  <li className="dropdown-header">
+                    <h6>Kevin Anderson</h6>
+                    <span>Administrador</span>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+
+                  <li>
+                    <a
+                      className="dropdown-item d-flex align-items-center"
+                      href="users-profile.html"
+                    >
+                      <i className="bi bi-person"></i>
+                      <span>Mi perfil</span>
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+
+                  <li>
+                    <a
+                      className="dropdown-item d-flex align-items-center"
+                      href="#"
+                    >
+                      <i className="bi bi-box-arrow-right"></i>
+                      <span>Sign Out</span>
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {hamburguerMenu && (
+        <aside
+          id="sidebar"
+          className={`sidebar ${hamburguerMenu ? "menu-open" : ""}`}
+        >
+          <ul class="sidebar-nav" id="sidebar-nav">
+            <li class="nav-item">
+              <a class="nav-link collapsed">
+                <i class="bi bi-grid"></i>
+                <span>Cronograma</span>
+              </a>
+            </li>
+          </ul>
+        </aside>
+      )}
+    </>
+  );
+};
+
+export default Header;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 // import DashboardRouter from './DashboardRouter'
 // import PublicRouter from './PublicRouter'
 // import PrivateRouter from './PrivateRouter'
-import OlvidarContrasena from '../pages/OlvidarContrasena'
-import NotFound from '../components/notFound/NotFound'
-import Login from '../pages/Login'
-import PanelAdministrador from '../pages/PanelAdministrador'
-import Cronograma from '../pages/Cronograma'
+import OlvidarContrasena from "../pages/OlvidarContrasena";
+import NotFound from "../components/notFound/NotFound";
+import Login from "../pages/Login";
+import PanelAdministrador from "../pages/PanelAdministrador";
+import Cronograma from "../pages/Cronograma";
+import Layout from "../components/layout/Layout";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/login" element={<Navigate to="/" />}/>
-          <Route path="/olvidar-contrasena" element={<OlvidarContrasena />}/>
-          <Route path="/administrador" element={<PanelAdministrador />}/>
-          <Route path="/cronograma" element={<Cronograma />}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Navigate to="/" />} />
+        <Route path="/olvidar-contrasena" element={<OlvidarContrasena />} />
+        <Route element={<Layout />}>
+          <Route path="/administrador" element={<PanelAdministrador />} />
+          <Route path="/cronograma" element={<Cronograma />} />
           <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
-   </BrowserRouter>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
