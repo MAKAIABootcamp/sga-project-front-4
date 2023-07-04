32083b3f08b1c46e6d26f652ecd4d4a2e9c3db57
mario-cartagena <jhonmariocartagena@gmail.com>
2023-07-04 14:04:00 -0500

    separando acciones y ventanas modales

--- src/pages/Cronograma.jsx
+++ src/pages/Cronograma.jsx
@@ -1,49 +1,62 @@
-import React, { useEffect, useRef,useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import interactionPlugin from '@fullcalendar/interaction';
-import esLocale from '@fullcalendar/core/locales/es';
-import '../styles/cronograma/Cronograma.scss'
+import React, { useEffect, useRef, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+import esLocale from "@fullcalendar/core/locales/es";
+import "../styles/cronograma/Cronograma.scss";
+import AgregarEvento from "../components/cronograma/AgregarEvento";
 
 const Cronograma = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [eventName, setEventName] = useState('');
 
+  // Variable de estado para controlar la apertura y el cierre del modal
+  const [showModal, setShowModal] = useState(false);
+
+  // Para abrir el modal al darle click a un día
   const handleDayClick = () => {
-    setModalOpen(true);
+    setShowModal(true);
   };
 
+  // Para cerrar el modal
   const handleModalClose = () => {
-    setModalOpen(false);
-    setEventName('');
+    setShowModal(false);
   };
 
-  const handleEventSubmit = () => {
-    console.log(eventName);
+  // Para agregar un evento, acá debería ir la lógica del post a la base de datos, revisar el getAPI de full calendar
+  const handleAgregarEvento = (evento) => {
+    console.log(evento);
     handleModalClose();
   };
 
   const events = [
-    { title: 'Evento 1', date: '2023-07-14' },
-    { title: 'Evento 2', date: '2023-07-02' },
-    { title: 'Evento 3', date: '2023-07-03' },
+    { title: 'Evento 1', start: '2023-07-14T10:00:00', end: '2023-07-14T12:00:00' },
+    { title: 'Evento 2', start: '2023-07-02T15:30:00', end: '2023-07-02T16:30:00' },
+    { title: 'Evento 3', start: '2023-07-03T09:45:00', end: '2023-07-03T11:30:00' },
+    { title: "Evento 4", date: "2023-07-02" },
+    { title: "Evento 5", date: "2023-07-03" },
+    
   ];
 
   return (
-    <div>
-      <div className="container"  style={{ padding: '8rem' }}>
+    <section>
+      <div className="container" style={{ padding: "8rem" }}>
         <div id="calendar" className="custom-calendar">
           <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              listPlugin,
+              interactionPlugin,
+            ]}
+            timeZone= 'America/Bogota'
             initialView="dayGridMonth"
-            locale= {esLocale}
+            locale={esLocale}
             headerToolbar={{
-              left: 'prev next today',
-              center: 'title',
-              right: 'dayGridMonth timeGridWeek listWeek'
+              left: "prev next today",
+              center: "title",
+              right: "dayGridMonth timeGridWeek listWeek",
             }}
             events={events}
             editable={true}
@@ -51,7 +64,13 @@ const Cronograma = () => {
           />
         </div>
       </div>
-      <Modal show={modalOpen} onHide={handleModalClose}>
+      <AgregarEvento
+        show={showModal}
+        onClose={handleModalClose}
+        onAgregarEvento={handleAgregarEvento}
+      />
+
+      {/* <Modal show={modalOpen} onHide={handleModalClose}>
         <Modal.Header closeButton>
           <Modal.Title>Agregar evento</Modal.Title>
         </Modal.Header>
@@ -76,9 +95,9 @@ const Cronograma = () => {
            Eliminar 
           </Button>
         </Modal.Footer>
-      </Modal>
-    </div>
+      </Modal> */}
+    </section>
   );
-}
+};
 
 export default Cronograma;
