64ba15ada513a2885a9c60e0f610194136fd9026
luisa urrego <luisafernandaurrego89@gmail.com>
2023-07-19 22:47:16 -0500

    autenticacion

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,14 +1,22 @@
 import { login } from "../../services/getUsers";
+import { collections } from "../../services/data"
 
 export const loginActionAsync = (email, password) => {
   return async (dispatch) => {
     try {
       const response = await login(email, password);
       if (response) {
-        //Se dispara la accion sincrona
+        dispatch(loginActionSync(response));
       }
     } catch (error) {
       console.log(error);
     }
+  };
+};
+
+export const loginActionSync = (user) => {
+  return {
+    type: collections.USUARIOS,
+    payload: user,
   };
 };
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,28 @@
+import { collections } from "../../services/data";
+
+const initialValues = {
+    user: {},
+    error: null
+}
+
+
+const userReducer = (state = initialValues, action) => {
+    switch (action.type) {
+        case collections.USUARIOS :
+            
+          return {
+            ...state,
+            user: {
+                ...action.payload.user
+            },
+            error: action.payload.error
+          }
+    
+        default:
+            return {
+                ...state
+            }
+    }
+
+}
+export default userReducer
