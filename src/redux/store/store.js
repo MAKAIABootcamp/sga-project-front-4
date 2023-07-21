784bb284f51992254ecad67962aa758a50b86725
AngieMorenoOrtega <guaiagaleria@gmail.com>
2023-07-21 14:23:38 -0500

    corrigiendo stor

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,16 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
-import thunk from 'redux-thunk';
-import eventsReducer from "../reducers/eventsReducer";
-import adminRegisterReducer from "../reducers/adminRegisterReducer";
-
-const store = configureStore({
-    reducer: {eventsReducer, adminRegisterReducer},
-    middleware: [thunk],
-});
-
-export default store;
-=======
 import eventsReducer from "../reducers/eventsReducer";
 import adminRegisterReducer from "../reducers/adminRegisterReducer";
 import cursoReducer from "../reducers/cursosReducer";
@@ -24,4 +12,3 @@ const store = configureStore({
 });
 
 export default store;
->>>>>>> recursoseducativosfinal
